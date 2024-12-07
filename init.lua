2ed6ccfb15db2cc2126ef40dc119ac160042ad2c
krypek <krypek@tuta.io>
2024-12-07 12:34:04 +0100

    Update

--- init.lua
+++ init.lua
@@ -543,36 +543,36 @@ require('lazy').setup({
         'hrsh7th/nvim-cmp',
         event = 'InsertEnter',
         dependencies = {
-            {
-                'L3MON4D3/LuaSnip',
-                build = (function()
-                    -- Build Step is needed for regex support in snippets.
-                    -- This step is not supported in many windows environments.
-                    -- Remove the below condition to re-enable on windows.
-                    if vim.fn.has('win32') == 1 or vim.fn.executable('make') == 0 then return end
-                    return 'make install_jsregexp'
-                end)(),
-                dependencies = {
-                    {
-                        'rafamadriz/friendly-snippets',
-                        config = function() require('luasnip.loaders.from_vscode').lazy_load() end,
-                    },
-                },
-            },
-            'saadparwaiz1/cmp_luasnip',
+            -- {
+            --     'L3MON4D3/LuaSnip',
+            --     build = (function()
+            --         -- Build Step is needed for regex support in snippets.
+            --         -- This step is not supported in many windows environments.
+            --         -- Remove the below condition to re-enable on windows.
+            --         if vim.fn.has('win32') == 1 or vim.fn.executable('make') == 0 then return end
+            --         return 'make install_jsregexp'
+            --     end)(),
+            --     dependencies = {
+            --         {
+            --             'rafamadriz/friendly-snippets',
+            --             config = function() require('luasnip.loaders.from_vscode').lazy_load() end,
+            --         },
+            --     },
+            -- },
+            -- 'saadparwaiz1/cmp_luasnip',
 
             'hrsh7th/cmp-nvim-lsp',
             'hrsh7th/cmp-path',
         },
         config = function()
             local cmp = require('cmp')
-            local luasnip = require('luasnip')
-            luasnip.config.setup({})
+            -- local luasnip = require('luasnip')
+            -- luasnip.config.setup({})
 
             cmp.setup({
-                snippet = {
-                    expand = function(args) luasnip.lsp_expand(args.body) end,
-                },
+                -- snippet = {
+                --     expand = function(args) luasnip.lsp_expand(args.body) end,
+                -- },
                 completion = { completeopt = 'menu,menuone,noinsert' },
 
                 -- For an understanding of why these mappings were
