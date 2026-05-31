487456c3fac2c802e70c06a405622ab4bfeff02a
krypek <krypek@tuta.io>
2026-05-31 09:57:42 +0200

    Add callgraph.nvim

--- init.lua
+++ init.lua
@@ -831,6 +831,15 @@ require('lazy').setup({
         dependencies = { 'MunifTanjim/nui.nvim' },
         opts = {},
     },
+    { -- callgraph
+        'barreiroleo/callgraph.nvim',
+        opts = {},
+        keys = {
+            { '<leader>ci', function() require('callgraph').run { direction = 'in' } end, desc = 'Callgraph: incoming calls' },
+            { '<leader>co', function() require('callgraph').run { direction = 'out' } end, desc = 'Callgraph: outgoing calls' },
+            { '<leader>cm', function() require('callgraph').run { direction = 'mix' } end, desc = 'Callgraph: mixed calls' },
+        },
+    },
     -- language specific
     { -- lazydev.nvim
         'folke/lazydev.nvim',
